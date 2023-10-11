d944f222934e6ae3f3ac2de52ca52094406059e9
aresxrd <85245168+aresxrd@users.noreply.github.com>
2023-10-12 01:06:23 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // @ts-check
 
 module.exports = {
-  token: "MTE0ODk5Mjg4MTg5MDE2ODg3Mw.GqXOYV.niS7WGHZ3KKIOaWyb4qnhGXf4UTLEQawb7Bvzc",
+  token: "TOKEN GİRİN",
 };
 ////////////////////////////BLACK DEVELOPMENT
